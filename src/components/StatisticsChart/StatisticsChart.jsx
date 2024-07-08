26845dbaab12debd280a90569f87c5fbcdcb3e00
Shwetasingh77 <shwetasinghrajputbirpur@gmail.com>
2024-07-09 02:26:36 +0530

    update

--- src/components/StatisticsChart/StatisticsChart.jsx
+++ src/components/StatisticsChart/StatisticsChart.jsx
@@ -1,91 +1,93 @@
+import React from 'react'
 import ReactECharts from 'echarts-for-react'
 import * as echarts from 'echarts'
+
+
 const StatisticsChart = () => {
-
-    const option = {
-        color: ['var(--orange)'],
-
-        toolbox: {
+    const option ={
+        color:['var(--orange)'],
+        toolbox:{
             feature: {
-                saveAsImage: {},
+            saveAsImage:{},
             }
         },
-
-        tooltip: {
-            trigger: "axis",
+        tooltip:{
+            trigger:'axis',
             axisPointer: {
-                type: "cross"
+                type: 'cross',
             },
-            backgroundColor: "rgba(0, 0, 0, 0.59)",
-            borderWidth: 0,
+            backgroundColor:"rgba(0,0,0,0.59)",
+            borderWidth:0,
         },
-        grid: {
-            left: "3%",
-            right: "4%",
-            bottom: "3%",
-            containLabel: true,
-            show: false,
+        grid:{
+            left:'3%',
+            right:'4%',
+            bottom:'3%',
+            containLabel:true,
+            show:false,
         },
-
         xAxis: [
             {
-                type: "category",
-                boundaryGap: false,
-                data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+                type:"category",
+                boundaryGap:false,
+                data: ["Mon","Tue","Thu","Fri","Sat","Sun"]
             }
         ],
         yAxis: [
             {
-                type: "value",
-                splitLine: {
-                    show: false,
+                type:"value",
+                splitLine:{
+                    show:false,
                 }
             }
         ],
-        series: [
-            {
-                type: "line",
-                smooth: true,
-                lineStyle: {
-                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                        {
-                            offset: 0,
-                            color: "rgb(255, 191, 0)",
-                        },
-                        {
-                            offset: 1,
-                            color: "#F450D3"
-                        }
-                    ]),
-                    width: 4
-                },
-                areaStyle: {
-                    opacity: .5,
-                    color: new echarts.graphic.LinearGradient(0, 0, 0, 0.8, [
-                        {
-                            offset: 0,
-                            color: "#FE4C00"
-                        },
-                        {
-                            offset: 1,
-                            color: "rgba(255,144,70,0.1)"
-                        }
-                    ])
-                },
-                emphasis: {
-                    focus: "series",
-                },
-                showSymbol: false,
-                data: [28000, 19000, 32000, 18000, 41000, 30000, 26000]
-            }
-        ]
-    }
+         series:[
+           { 
+            type:"line",
+            smooth:true,
+            lineStyle: {
+                color: new echarts.graphic.LinearGradient(0,0,0,1,[
+                    {
+                        offset:0,
+                        color:"rgb(255,191,0)",
+                    },
+                    {
+                        offset:1,
+                        color:"#F450D3"
+                    }
+                ]),
+                width:4
 
-    return (
-        <div style={{ width: '100%', height: '150px', margin: 'auto' }}>
-        <ReactECharts option={option} style={{ height: '90%', width: '100%' }} />
-    </div>
-    )
+            },
+            areaStyle:{
+                opacity:.5,
+                color: new echarts.graphic.LinearGradient(0,0,0,0.8, [
+                    {
+                        offset:0,
+                        color:"#FE4C00"
+                    },
+                    {
+                        offset:1,
+                        color:"rgba(255,144,70,0.1)"
+                    }
+                ])
+
+            },
+            emphasis:{
+                focus:"series",
+                
+            },
+            showSymbol:false,
+            data:[28000,19000,32000,18000,41000,30000,26000]
+           }
+
+         ]
+    }
+  return (
+    <div style={{ width: '100%', height: '250px', margin: 'auto' }}>
+            <ReactECharts option={option} style={{ height: '100%', width: '100%' }} />
+        </div>
+  )
 }
 
 export default StatisticsChart
